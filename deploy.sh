a233847b0a020c5889fc7edbcaec04096af2ffb4
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-12-23 18:18:11 -0500

    moar

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,25 @@
 # separate repository where the deployed site lives.
 
 echo -e "\\033[0;32mDeploying updates to GitHub...\\033[0m"
-hugo
 
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
 cd public
+
+# Add changes to git.
 git add .
 
+# Commit changes.
 msg="Rebuilding portfolio: `date`"
 
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 
+# Push source and build repos.
 git commit -m "$msg"
+
 git push origin master
 cd ..
