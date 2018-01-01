b13b7c2190f6373c87558c71129a52e031183b43
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2018-01-01 00:57:43 -0500

    moar

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,27 @@
 # the changes inside of the /public folder. I have a
 # separate repository where the deployed site lives.
 
+# Set the English locale for the `date` command.
+export LC_TIME=en_US.UTF-8
+
 echo -e "\\033[0;32mDeploying updates to GitHub...\\033[0m"
+
+if [[ $(git status -s) ]]; then
+  echo "The working directory is dirty, please commit or stash any pending changes"
+  exit 1;
+fi
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+(cd public || exit)
 
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="Rebuilding portfolio: `date`"
+msg="Rebuilding portfolio site: $(date)"
 
 if [ $# -eq 1 ]
   then msg="$1"
@@ -26,4 +34,4 @@ fi
 git commit -m "$msg"
 
 git push origin master
-cd ..
+(cd .. || exit)
