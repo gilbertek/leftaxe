9d8d547c03748340d451a2bb3b9c0485ce809e19
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2018-05-28 17:54:10 -0400

    Some new posts

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ SOURCE_DIR=~/Projects/BLOG/leftaxe/
 # Set the English locale for the `date` command.
  export LC_TIME=en_US.UTF-8
 
-# if [[ $(git status -s) ]]
-# then
-#   echo "The working directory is dirty. Please commit any pending changes."
-#   exit 1;
-# fi
+if [[ $(git status -s) ]]
+then
+  echo "The working directory is dirty. Please commit any pending changes."
+  exit 1;
+fi
 
 if [ ! -d "$DEPLOY_DIR" ]; then
   echo "Deploy directory '$DEPLOY_DIR' does not exist. Aborting." >&2
@@ -42,4 +42,4 @@ git commit -m "$msg"
 echo "pushing site"
 git push origin master
 cd $SOURCE_DIR
-echo "deployed leftaxe to https://gilbertek.github.com"
+echo "deployed leftaxe to https://gilbertek.github.io"
