bb024b7851f37ccaae2877b0ce6c47c4869d04c5
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2018-10-01 17:08:22 -0400

    Update link to fix dups error

--- deploy.sh
+++ deploy.sh
@@ -12,18 +12,12 @@ SOURCE_DIR=~/Projects/BLOG/leftaxe/
 # Set the English locale for the `date` command.
  export LC_TIME=en_US.UTF-8
 
-if [[ $(git status -s) ]]
-then
-  echo "The working directory is dirty. Please commit any pending changes."
-  exit 1;
-fi
-
 if [ ! -d "$DEPLOY_DIR" ]; then
-  echo "Deploy directory '$DEPLOY_DIR' does not exist. Aborting." >&2
+  echo "\\033[0;32mDeploy directory '$DEPLOY_DIR' does not exist. Aborting.\\033[0m" >&2
   return 1
 fi
 
-echo "Generating site"
+echo "\\033[0;32m\\nGenerating site\\033[0m"
 HUGO_ENV="production" hugo -d $DEPLOY_DIR
 
 # Go To Public site folder
@@ -31,6 +25,8 @@ cd $DEPLOY_DIR
 
 # Commit changes.
 msg="Rebuilding portfolio site: $(date)"
+
+git add --all
 
 if [ $# -eq 1 ]
   then msg="$1"
@@ -42,4 +38,4 @@ git commit -m "$msg"
 echo "pushing site"
 git push origin master
 cd $SOURCE_DIR
-echo "deployed leftaxe to https://gilbertek.github.io"
+echo "\\033[0;32mdeployed leftaxe to https://gilbertek.github.io \\033[0m"
